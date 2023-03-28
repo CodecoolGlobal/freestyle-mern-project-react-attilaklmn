a2606c7678cf268d0c0b83255e567e2b7010fe05
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-03-28 15:09:28 +0200

    add Schema and request handling

--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
+const fetchCardList = () => {
+  return fetch(
+    "http://localhost:8080/api/cards/filter/?health=4&sort=artistName&sortOrder=desc"
+  ).then((res) => res.json());
+};
+
+const logCurrent = async () => {
+  console.log(await fetchCardList());
+};
+
 const Main = () => {
+  useEffect(() => {
+    logCurrent();
+  }, []);
   return <div>Main</div>;
 };
 
