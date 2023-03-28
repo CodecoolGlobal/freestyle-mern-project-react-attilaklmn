c394a1210557a8ea6831169729dfa331cc9b7f58
attilaklmn <kalmanattila04@gmail.com>
2023-03-28 22:37:47 +0200

    add pages to cardlist component

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,9 +1,16 @@
 import "./Card.css"
 
 const Card = (props) => {
+
+  const handleAddToFavorites = event => {
+    event.preventDefault();
+    console.log(`Adding card with ID ${props.card._id} to favorites`)
+  }
+
   return <div className="card">
     <img src={props.card.image} alt="" />
     {props.card.name}
+    <button type="button" onClick={handleAddToFavorites} >Add to favorites!</button>
     </div>;
 };
 
--- client/src/Pages/CardList.jsx
+++ client/src/Pages/CardList.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import "./CardList.css"
 import Card from "../Components/Card";
+import Pagination from "../Components/Card/Pagination";
 
-
+let PageSize = 12;
 
 const CardList = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [cardList, setCardList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const fetchCardList = () => {
     setIsLoading(true)
@@ -14,8 +16,8 @@ const CardList = () => {
       .then((res) => res.json())
       .then((cards) => {
         return cards.filter(card => {
-          if (card.cardSetId === 4) {
-            return card
+          if (card.collectible === 1) {
+              return card
           }
         });
       });
@@ -23,22 +25,70 @@ const CardList = () => {
 
   useEffect(() => {
     fetchCardList().then((cards) => {
-      setIsLoading(false)
       setCardList(cards)
+      setIsLoading(false)
     });
   }, []);
 
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return cardList.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, isLoading]);
+
   return (
-    <div className="cardlist">
+    <div className="content">
+      {!isLoading && <div className="filters" >
+        Filters.
+      </div>}
+      <div className="cardlist">
       {!isLoading ? (
-        cardList.map((card, index) => {
+        currentTableData.map((card, index) => {
           return <Card key={index} card={card}></Card>;
         })
-      ) : (
-        <div>Loading...</div>
+        ) : (
+          <div className="loading">Loading...</div>
       )}
+          </div>
+        {!isLoading && <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={cardList.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />}
+
     </div>
   );
 };
 
 export default CardList;
+
+
+/*
+//////// cardID counter://////////
+//before return:
+const cardIDs = {};
+  const cardIDsArray = []
+
+  const showIDs = () => {
+    for (const [key, value] of Object.entries(cardIDs)) {
+          cardIDsArray.push(`ID: ${key}: ${value} darab`)
+    }
+  }
+
+  //inside jsx map:
+  if (cardIDs[`${card.cardSetId}`]) {
+            cardIDs[`${card.cardSetId}`]++;
+          } else {
+            cardIDs[`${card.cardSetId}`] = 1;
+          }
+
+  //after ternary in jsx:
+{showIDs()}
+      {cardIDsArray.map(id => {
+        return (
+          <div>{id}</div>
+        )
+      })}
+*/
