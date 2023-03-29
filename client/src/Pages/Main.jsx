46c35aab3e33606981a7fd6653d309117fc01268
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-03-29 20:44:31 +0200

    :D

--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -1,5 +1,8 @@
 import gabicard from "../gabicard.png";
 import aticard from "../aticard.png";
+import bogrecard from "../bogrecard.png";
+import codecoolcard from "../codecoolcard.png";
+import cutecatcard from "../cutecatcard.png";
 
 import "./Main.css";
 
@@ -11,6 +14,19 @@ const Main = () => {
       </div>
       <div className="atidiv">
         <img className="aticard" alt="ati" src={aticard}></img>
+      </div>
+      <div className="bogrediv">
+        <img className="bogrecard" alt="bogre" src={bogrecard}></img>
+      </div>
+      <div className="codecooldiv">
+        <img
+          className="codecoolcard"
+          alt="codecoolcard"
+          src={codecoolcard}
+        ></img>
+      </div>
+      <div className="cutecatdiv">
+        <img className="cutecatcard" alt="cutecatcard" src={cutecatcard}></img>
       </div>
     </div>
   );
